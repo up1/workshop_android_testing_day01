7b18eb106743c07c8dd6eae71dcb601c5b16de36
up1 <somkiat.p@gmail.com>
2019-04-25 22:11:28 +0700

    Working with Robolectric

--- app/src/test/java/com/example/myapplication/MainActivityRobolectricTest.java
+++ app/src/test/java/com/example/myapplication/MainActivityRobolectricTest.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.widget.Button;
+import android.widget.EditText;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,6 +24,10 @@ public class MainActivityRobolectricTest {
                 .get();
 
         // Act
+        EditText username = activity.findViewById(R.id.et_username);
+        username.setText("admin");
+        EditText password = activity.findViewById(R.id.et_passsword);
+        password.setText("password");
         Button button = activity.findViewById(R.id.btn_signin);
         button.performClick();
 
@@ -30,6 +35,7 @@ public class MainActivityRobolectricTest {
         Intent expectedIntent = new Intent(activity, ResultActivity.class);
         Intent actual = shadowOf(activity).getNextStartedActivity();
         assertEquals(expectedIntent.getComponent(), actual.getComponent());
+        assertEquals("Success!!!", actual.getStringExtra("RESULT"));
     }
 
 }
